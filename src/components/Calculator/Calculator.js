ab7e30572661c6c166dbd8929ac38ff94390bcfd
Alex Buyanov <alex.bu.89@gmail.com>
2017-02-26 23:47:32 +0100

    init calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -3,6 +3,31 @@ import { connect } from 'react-redux'
 import { test } from '../../store/calculator'
 import './Calculator.sass'
 
+const calculatorConf = [
+  {
+    category: 'boiler',
+    title: 'Выбор котла',
+    products: [
+      { title: 'Газовый одноконтурный', price: 1000, service_price: 100, type: { form: 'checkbox' }, desc: '', img_src: '#' },
+      { title: 'Газовый двухконтурный', price: 1000, service_price: 100, type: { form: 'checkbox' }, desc: '', img_src: '#' },
+      { title: 'Электрический', price: 1000, service_price: 100, type: { form: 'checkbox' }, desc: '', img_src: '#' },
+      { title: 'Дизельный', price: 1000, service_price: 100, type: { form: 'checkbox' }, desc: '', img_src: '#' },
+      { title: 'Твердотопливный', price: 1000, service_price: 100, type: { form: 'checkbox' }, desc: '', img_src: '#' },
+      { title: 'Бойлер косвенного нагрева до 200л', price: 1000, service_price: 100, type: { form: 'checkbox' }, desc: '', img_src: '#' },
+      { title: 'Бойлер косвенного нагрева более 200л', price: 1000, service_price: 100, type: { form: 'checkbox' }, desc: '', img_src: '#' },
+      { title: 'Энергоаккумулятор до 500л', price: 1000, service_price: 100, type: { form: 'checkbox' }, desc: '', img_src: '#' },
+    ]
+  },
+  {
+    category: 'test',
+    title: 'Test',
+    products: [
+      { title: 'Газовый одноконтурный', price: 1000, service_price: 100, type: { form: 'checkbox' }, desc: '', img_src: '#' },
+      { title: 'Газовый двухконтурный', price: 1000, service_price: 100, type: { form: 'checkbox' }, desc: '', img_src: '#' },
+    ]
+  }
+]
+
 class Calculator extends Component {
 
   render () {
@@ -13,7 +38,40 @@ class Calculator extends Component {
             <div className='col-lg-12 text-center'>
               <h2 className='section-heading'>Расчет цены</h2>
               <hr className='primary' />
-              <button onClick={ this.props.test }>TEST</button>
+
+              <ul className='nav nav-tabs' role='tablist'>
+                <li className='nav-item'>
+                  <a className='nav-link active' data-toggle='tab' href='#home' role='tab'>Выбор котла</a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link' data-toggle='tab' href='#profile' role='tab'>Выбор домохода</a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link' data-toggle='tab' href='#messages' role='tab'>Тест</a>
+                </li>
+              </ul>
+
+              <div className='row text-justify'>
+                <div className='col-7'>
+                  <div className='tab-content'>
+                    <div className='tab-pane active' id='home' role='tabpanel'>
+                      <form>
+                        <label className='custom-control custom-radio'>
+                          <input id='radio1' name='radio' type='radio' className='custom-control-input' />
+                          <span className='custom-control-indicator'></span>
+                          <span className='custom-control-description'>Toggle this custom radio</span>
+                        </label>
+                      </form>
+                    </div>
+                    <div className='tab-pane' id='profile' role='tabpanel'>2</div>
+                    <div className='tab-pane' id='messages' role='tabpanel'>3</div>
+                  </div>
+                </div>
+                <div className='col-5'>
+                  Итого
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
